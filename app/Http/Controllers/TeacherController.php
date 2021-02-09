6e550b2511939c0198ca6a3d04ac88fd1aabadb8
Amiel Titular <amotitular@gmail.com>
2021-02-09 14:07:17 +0800

    first commit

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Teacher;
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    public function getTeachers(){
+        return response()->json(Teacher::get(), 200);
+    }
+}

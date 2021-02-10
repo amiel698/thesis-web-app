f7d21987808480e4274cd05dbfb103356e530b47
Amiel Titular <amotitular@gmail.com>
2021-02-10 22:27:04 +0800

    resource edited

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('teachers', 'TeacherController');
 Route::get('teachers/{id}', 'TeacherController');
-Route::post('teachers', 'TeacherController@addTeacher');
+Route::post('teachers', 'TeacherController');
 
 
 

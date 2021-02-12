f7310c2b3781e0ab3973f08958fbcd6f17ab5921
Amiel Titular <amotitular@gmail.com>
2021-02-12 11:47:28 +0800

    delete added

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -79,8 +79,9 @@ class TeacherController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request, Teacher $teacher)
     {
-        //
+        $teacher->delete();
+        return response()->json(null);
     }
 }

ba56f8c47eacc3cf2619258718aa65ebf2b7d19a
Amiel Titular <amotitular@gmail.com>
2021-02-21 21:06:46 +0800

    studcontroller changed

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -100,7 +100,7 @@ class StudentController extends Controller
             $students = Students::get();
             foreach($students as $student){
                 if($student_id == $student->student_id and $password == $student->password){
-                    return response()->json('Logged In');
+                    return response()->json('httpStatus');
                 }
                 else if($student_id == $student->student_id and $password != $student->password){
                     return response()->json('Wrong Password');

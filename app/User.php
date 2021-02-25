9e666ef2bd7e32ab9f8bccc783a883fea7b42289
Amiel Titular <amotitular@gmail.com>
2021-02-25 22:06:40 +0800

    register.blade.php changes

--- app/User.php
+++ app/User.php
@@ -8,29 +8,12 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    
     protected $fillable = [
         'first_name', 'last_name', 'user_name', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
 }

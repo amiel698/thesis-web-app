717b2b569fd530fd6fb425fddbb05d85d73cc716
Amiel Titular <amotitular@gmail.com>
2021-02-28 15:24:49 +0800

    2nd update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,7 +47,7 @@
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password">
 
-                            
+
                             </div>
                         </div>
 
@@ -62,7 +62,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                   Re
                                 </button>
                             </div>
                         </div>

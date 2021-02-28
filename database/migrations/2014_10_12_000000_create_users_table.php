e2edc01020fe135993112e55a82df401b4c3431b
Amiel Titular <amotitular@gmail.com>
2021-02-28 20:03:14 +0800

    migration changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->id();
             $table->string('user_name')->primary()->unique();
             $table->string('password');
             $table->string('first_name');

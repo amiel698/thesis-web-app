2b87ce757d59b89a88492616f9d85700cb60b709
Amiel Titular <amotitular@gmail.com>
2021-02-28 12:45:10 +0800

    UserController added

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::post('register', 'Auth\RegisterController@create');
+Route::post('register', 'UserController@registerUser');

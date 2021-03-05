a81955af681505ab11beec98b3e7e3e3dea5fc4c
Amiel Titular <amotitular@gmail.com>
2021-03-05 14:42:30 +0800

    layouts changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <style>
-        .box{
-            width:600px;
-            margin:0 auto;
-            border:1px solid #ccc;
-        }
-    </style>
-</head>
-<body>
-    <br/>
-    <div class="jumbotron text-center">
-        <h3 align="center">Welcome Please Log-in</h3><br/>
-            <div class="login-register-btn">
-                <a class="btn btn-primary" href="{{ route('login')}}" role="button">Login</a>
-                <a class="btn btn-primary" href="{{ route('register')}}" role="button">Register</a>
-            </div>
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+
+<div class="jumbotron text-center">
+    <h3 align="center">Welcome Please Log-in</h3><br/>
+        <div class="login-register-btn">
+            <a class="btn btn-primary" href="{{ route('login')}}" role="button">Login</a>
+            <a class="btn btn-primary" href="{{ route('register')}}" role="button">Register</a>
+        </div>
+</div>
+
+@endsection
+
+

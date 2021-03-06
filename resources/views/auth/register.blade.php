41e1b178aef18df3239f95a105f5050232f75f49
Amiel Titular <amotitular@gmail.com>
2021-03-06 16:19:43 +0800

    new Changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,9 +8,8 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{route('register')}}">
+                    <form method="POST" action="{{route('create')}}">
                         @csrf
-
                         <div class="form-group row">
                             <label for="first_name" class="col-md-4 col-form-label text-md-right">{{ __('First Name') }}</label>
                             <div class="col-md-6">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -47,7 +48,7 @@
                         @guest
 
                             @if (Route::has('register'))
-                        
+
                             @endif
                         @else
                             <li class="nav-item dropdown">

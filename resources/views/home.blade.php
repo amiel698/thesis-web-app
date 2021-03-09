982732eacfbd3e511de1c7eff221440553494fae
Amiel Titular <amotitular@gmail.com>
2021-03-09 12:15:55 +0800

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -71,55 +71,33 @@
                 </div>
             </div>
         </nav>
+    </div>
 
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
 
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                        You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
-
-
-
-
-
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
+
+
 </body>
 </html>
 
 
-{{--  <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">Dashboard</div>
-
-                        <div class="card-body">
-                            @if (session('status'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
-                                </div>
-                            @endif
-
-                            You are logged in!
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>  --}}
 
 
 

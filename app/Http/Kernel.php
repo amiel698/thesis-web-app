66432597c75e66eb694a28d028ccb67a11f7822d
Amiel Titular <amotitular@gmail.com>
2021-03-09 16:15:26 +0800

    test

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,7 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'AlreadyLoggedIn' => \App\Http\Middleware\AlreadyLoggedIn::class,
-        'isLogged' => \App\Http\Middleware\AuthCheck::class,
+        'AuthCheck' => \App\Http\Middleware\AuthCheck::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,

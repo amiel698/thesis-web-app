66755a64b0473ec5958d786e6ae146518f37b105
Amiel Titular <amotitular@gmail.com>
2021-03-09 18:24:27 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Auth::routes();
 //GET
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('logout', 'UserController@logout')->name('logout');
-Route::get('data', 'UserController@index');
+Route::get('data', 'UserController@index')->name('data');
 
 //POST
 Route::post('check', 'UserController@check')->name('check');

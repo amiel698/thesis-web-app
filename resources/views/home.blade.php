65c3b9aac3947439838f0d3c643e814461af195c
Amiel Titular <amotitular@gmail.com>
2021-03-09 18:14:52 +0800

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,7 +63,7 @@
                               </tr>
                             </thead>
                             <tbody>
-                                @foreach($users as $user)
+                                @foreach ($users as $user)
                               <tr>
                                 <td>{{ $user->first_name }}</td>
                                 <td>{{ $user->last_name }}</td>

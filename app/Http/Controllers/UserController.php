8ca9077f382f2f9a42cb58e887de053b3eea435c
Amiel Titular <amotitular@gmail.com>
2021-03-09 19:28:45 +0800

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -38,7 +38,7 @@ class UserController extends Controller
     // }
 
     public function index(){
-        $users = User::all();
+        $users = User::get();
         return view('home', ['users'=>$users]);
     }
 

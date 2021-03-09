a13d8bde82d7d419eca164c41c1971f057bf4007
Amiel Titular <amotitular@gmail.com>
2021-03-09 17:38:07 +0800

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,7 +55,32 @@
                 <div class="card">
                     <div class="card-header">Student List</div>
                     <div class="card-body">
-                        You are logged in!
+                        <table class="table table-striped">
+                            <thead>
+                              <tr>
+                                <th>Firstname</th>
+                                <th>Lastname</th>
+                                <th>Email</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>John</td>
+                                <td>Doe</td>
+                                <td>john@example.com</td>
+                              </tr>
+                              <tr>
+                                <td>Mary</td>
+                                <td>Moe</td>
+                                <td>mary@example.com</td>
+                              </tr>
+                              <tr>
+                                <td>July</td>
+                                <td>Dooley</td>
+                                <td>july@example.com</td>
+                              </tr>
+                            </tbody>
+                          </table>
                     </div>
                 </div>
             </div>

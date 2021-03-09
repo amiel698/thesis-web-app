79aba4004d1a12844aab96efb41639e358534010
Amiel Titular <amotitular@gmail.com>
2021-03-09 18:28:01 +0800

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -39,7 +39,7 @@ class UserController extends Controller
 
     public function index(){
         $users = User::all()->where('user_type', 1);
-        return view('home', ['users'=>$users]);
+        return view('home', compact('users'));
     }
 
 

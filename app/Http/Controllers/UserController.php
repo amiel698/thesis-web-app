6827e745189d87c65e41d4ba1a756b9dacd1a864
Amiel Titular <amotitular@gmail.com>
2021-05-07 15:27:36 +0800

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use app\Charts\ChartTest;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        $teacher = User::where('user_type', 2)->get()->count();
+        
+    }
+
+
 }

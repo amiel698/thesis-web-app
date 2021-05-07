23d06c7ec1120cdd9b15d7fc5cead5072d90740f
Amiel Titular <amotitular@gmail.com>
2021-05-07 16:35:24 +0800

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,8 @@
         </div>
     </div>
 </div>
+@endsection
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
 {!! $chart_test->script() !!}
-@endsection
+
 

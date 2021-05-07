8c290aa903bab4ebbca62fb410e344acf9f2797a
Amiel Titular <amotitular@gmail.com>
2021-05-07 17:31:41 +0800

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,13 +27,39 @@ class HomeController extends Controller
      */
 
         public function index(){
+
+            $borderColors = [
+                "rgba(255, 99, 132, 1.0)",
+                "rgba(22,160,133, 1.0)",
+                "rgba(255, 205, 86, 1.0)",
+                "rgba(51,105,232, 1.0)",
+                "rgba(244,67,54, 1.0)",
+                "rgba(34,198,246, 1.0)",
+                "rgba(153, 102, 255, 1.0)",
+                "rgba(255, 159, 64, 1.0)",
+                "rgba(233,30,99, 1.0)",
+                "rgba(205,220,57, 1.0)"
+            ];
+            $fillColors = [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(22,160,133, 0.2)",
+                "rgba(255, 205, 86, 0.2)",
+                "rgba(51,105,232, 0.2)",
+                "rgba(244,67,54, 0.2)",
+                "rgba(34,198,246, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+                "rgba(233,30,99, 0.2)",
+                "rgba(205,220,57, 0.2)"
+
+            ];
+
             $teacher = User::where('user_type', 2)->get(DB::raw('COUNT(*) as count'))->pluck('count');
             $student = User::where('user_type', 1)->get(DB::raw('COUNT(*) as count'))->pluck('count');
 
             $chart_test = new AdminChart();
             $chart_test->labels(['Student', 'Teacher']);
-            $chart_test->dataset('Users', 'doughnut', $teacher->values())->color('red');
-            $chart_test->dataset('Users', 'doughnut', $student->values())->color('blue');
+            $chart_test->dataset('Users', 'doughnut', [$teacher->values(), $student->values()])->color($borderColors)->backgroundcolor($fillColors);
 
             return view('home', ['chart_test' => $chart_test]);
         }

67d9ff1ac6348c158cc8d2cd2698aa600fef0a45
Amiel Titular <amotitular@gmail.com>
2021-05-07 19:58:05 +0800

    test

--- resources/views/student/chart.blade.php
+++ resources/views/student/chart.blade.php
@@ -8,45 +8,7 @@
         <div class="col-md-12">
             <div class="card-header">{{ $student->first_name }} {{ $student->last_name }} Progress</div>
                 <div class="card-body">
-                    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                          <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                          <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                        </ol>
-                        <div class="carousel-inner">
-                          <div class="carousel-item active">
-                                {!! $chart->container()!!}
-                            <div class="carousel-caption d-none d-md-block">
-                              <h5>First slide label</h5>
-                              <p>Some representative placeholder content for the first slide.</p>
-                            </div>
-                          </div>
-                          <div class="carousel-item">
-                            {!! $chart_medium->container() !!}
-                            <div class="carousel-caption d-none d-md-block">
-                              <h5>Second slide label</h5>
-                              <p>Some representative placeholder content for the second slide.</p>
-                            </div>
-                          </div>
-                          <div class="carousel-item">
-                            {!! $chart_hard->container() !!}
-                            <div class="carousel-caption d-none d-md-block">
-                              <h5>Third slide label</h5>
-                              <p>Some representative placeholder content for the third slide.</p>
-                            </div>
-                          </div>
-                        </div>
-                        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span class="sr-only">Next</span>
-                        </a>
-                      </div>
-                    {{--  <div class="w-1/2">
+                    <div class="w-1/2">
                         {!! $chart->container()!!}
                     </div>
                     <div class="w-1/2">
@@ -54,7 +16,7 @@
                     </div>
                     <div class="w-1/2">
                         {!! $chart_hard->container() !!}
-                    </div>  --}}
+                    </div>
                 </div>
             </div>
         </div>

bb10f311f262d009aca3929a3701e635b79ae44e
Amiel Titular <amotitular@gmail.com>
2021-05-07 15:51:37 +0800

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
-use app\Charts\ChartTest;
+use App\Charts\ChartTest;
 use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller

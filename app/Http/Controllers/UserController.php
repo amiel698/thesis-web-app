32bb4fd8cf24bdfbcfbc8b658dce991c61d580e5
Amiel Titular <amotitular@gmail.com>
2021-05-07 17:08:38 +0800

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,27 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Charts\AdminChart;
 use Illuminate\Http\Request;
 use App\User;
 use App\Charts\ChartTest;
-use App\Charts\AdminChart;
 use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function index(){
-        $teacher = User::select(DB::raw("COUNT(*) as count"))
-        ->where('user_type', 2)
-        ->pluck('count');
-        $student = User::select(DB::raw("COUNT(*) as count"))
-        ->where('user_type', 2)
-        ->pluck('count');
+        $teacher = User::where('user_type', 2)->get()->count();
+        $student = User::where('user_type', 1)->get()->count();
 
-        $chart_test = new AdminChart;
+        $chart_test = new AdminChart();
         $chart_test->labels(['Jan', 'Feb', 'Mar']);
-        $chart_test->dataset('Users', 'doughnut', $teacher);
+        $chart_test->dataset('Users', 'doughnut', [$teacher->values(), $student->values()]);
 
-        return view('home')->with(['chart_test' => $chart_test]);
+        return view('home', compact('chart_test'));
     }
 
 

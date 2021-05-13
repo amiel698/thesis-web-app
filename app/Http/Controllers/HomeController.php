5922b77059437d1c1f954c7a6bc1850483341ad0
Amiel Titular <amotitular@gmail.com>
2021-05-13 17:31:17 +0800

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -54,7 +54,7 @@ class HomeController extends Controller
             }
             $query = Students::with(['info'])->whereTeacherUsersId($teacher->id);
             $stud_id = Students::with(['info'])->whereTeacherUsersId($teacher->id)->pluck('student_users_id');
-            dd($stud_id);
+            $score = StudentRecords::where('student_id', $stud_id)->pluck();
             $rows = $query->orderBy('created_at', 'ASC')->paginate(50);
             
            

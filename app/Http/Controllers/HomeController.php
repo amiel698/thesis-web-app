c0ededbb705c95e6eef35f89a7c7e08dc906cec8
Amiel Titular <amotitular@gmail.com>
2021-05-13 18:14:49 +0800

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,7 +55,7 @@ class HomeController extends Controller
             }
             $query = Students::with(['info'])->whereTeacherUsersId($teacher->id);
             $stud_id = Students::with(['info'])->whereTeacherUsersId($teacher->id)->pluck('student_users_id');
-            $score = StudentRecords::where('student_id', $stud_id)->select(DB::raw('AVG(score) as score_avg'))->pluck('score_avg');
+            $score = StudentRecords::where('student_id', $stud_id)->pluck('score')->average();
             dd($score);
             $rows = $query->orderBy('created_at', 'ASC')->paginate(50);
             
